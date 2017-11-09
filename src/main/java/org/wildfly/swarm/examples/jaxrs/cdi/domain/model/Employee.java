083eb02d40f5d4fd0346912ba945c092c7ee9090
Petr Pravda <petr.pravda@gmail.com>
2017-11-09 20:59:33 +0100

    initial commit

---
+++ src/main/java/org/wildfly/swarm/examples/jaxrs/cdi/domain/model/Employee.java
@@ -0,0 +1,39 @@
+package org.wildfly.swarm.examples.jaxrs.cdi.domain.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employee{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
